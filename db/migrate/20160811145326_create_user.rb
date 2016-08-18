a8f6c389f740fcc09ff32e99b81432a8170a0cd6
Brianna <bripeticca@gmail.com>
2016-08-18 10:14:01 -0400

    Updated user migration & added user model

--- db/migrate/20160811145326_create_user.rb
+++ db/migrate/20160811145326_create_user.rb
@@ -1,11 +1,11 @@
 class CreateUser < ActiveRecord::Migration
   def change
-    create_table :users do |t|
-      t.string :name
-      t.string :age
-      t.text :bio
-      t.string :dateofbirth
-      t.timestamps
+    create_table :users do |u|
+      u.string :name
+      u.string :age
+      u.text :bio
+      u.string :dateofbirth
+      u.timestamps
     end
   end
 end
